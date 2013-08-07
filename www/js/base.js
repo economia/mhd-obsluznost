8f2c003b46927fa0ca06fffd8378f9c95025105a
Marcel Šulek <marcel@sulek.eu>
2013-08-07 16:59:19 +0200

    spocitani nejvyssi hodnoty v souboru

--- www/js/base.js
+++ www/js/base.js
@@ -6,7 +6,9 @@
     function Serviceability(srcAddress, $parent){
       var this$ = this;
       this.$parent = $parent;
-      this.loadData(srcAddress, function(){});
+      this.loadData(srcAddress, function(){
+        return this$.computeStatistics(this$.data);
+      });
     }
     prototype.loadData = function(srcAddress, cb){
       var this$ = this;
@@ -14,6 +16,19 @@
         this$.data = data;
         return cb();
       });
+    };
+    prototype.computeStatistics = function(data){
+      var values, i$, len$, dayIndex, day, j$, len1$, dayValues;
+      values = [];
+      for (i$ = 0, len$ = data.length; i$ < len$; ++i$) {
+        dayIndex = i$;
+        day = data[i$];
+        for (j$ = 0, len1$ = day.length; j$ < len1$; ++j$) {
+          dayValues = day[j$];
+          values.push(dayValues);
+        }
+      }
+      return this.maxValue = Math.max.apply(Math, values);
     };
     return Serviceability;
   }());
--- www/ls/base.ls
+++ www/ls/base.ls
@@ -1,12 +1,19 @@
 class Serviceability
     (srcAddress, @$parent) ->
         <~ @loadData srcAddress
+        @computeStatistics @data
 
     loadData: (srcAddress, cb) ->
         (data) <~ $.getJSON "data/#srcAddress"
         @data = data
         cb!
 
+    computeStatistics: (data) ->
+        values = []
+        for day, dayIndex in data
+            for dayValues in day
+                values.push dayValues
+        @maxValue = Math.max ...values
 
 
 serviceability = new Serviceability do
