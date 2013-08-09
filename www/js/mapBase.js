304824a5781f57b4377e56e830d86ac12d1b5ec3
Marcel Šulek <marcel@sulek.eu>
2013-08-09 18:45:52 +0200

    zakladni vizualizace rozdilu po zastavkach na mape

--- www/js/mapBase.js
+++ www/js/mapBase.js
@@ -3,8 +3,7 @@
   window.map = L.map('map', {
     fadeAnimation: false,
     minZoom: 6,
-    maxZoom: 14,
-    maxBounds: [[50.356, 14.128], [49.693, 15.381]]
+    maxZoom: 14
   });
   x$ = map;
   x$.setView([50.08, 14.4], 12);
@@ -13,6 +12,26 @@
     attribution: '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/" target = "_blank">CC BY-NC-SA 3.0 CZ</a> IHNED.cz, mapové data &copy; <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>, <a target="_blank" href="http://www.infoprovsechny.cz/request/aktuln_verze_gtfs#incoming-988">dopravní data</a> <a target="_blank" href="http://dpp.cz">DPP Praha</a>'
   }));
   $.getJSON('./data/stationCoordinates.json', function(stationCoordinates){
-    return window.stationCoordinates = stationCoordinates;
+    return $.getJSON('./data/stopDifferences.json', function(stopDifferences){
+      var color;
+      color = d3.scale.linear().domain([-2000, 0, 2000]).range(['#D7191C', '#FFFFBF', '#1A9641']);
+      window.stationCoordinates = stationCoordinates;
+      return stationCoordinates.forEach(function(coord, id){
+        var diff, markerColor, x$;
+        if (coord) {
+          diff = stopDifferences[id];
+          markerColor = color(diff);
+          x$ = new L.CircleMarker(coord, {
+            fillColor: markerColor,
+            color: 'black',
+            strokeOpacity: 0.5,
+            strokeWidth: 0.5,
+            fillOpacity: 0.9
+          });
+          x$.addTo(map);
+          return x$;
+        }
+      });
+    });
   });
 }).call(this);
--- www/ls/mapBase.ls
+++ www/ls/mapBase.ls
@@ -3,11 +3,27 @@ window.map = L.map do
     *   fadeAnimation: false,
         minZoom: 6,
         maxZoom: 14
-        maxBounds: [[50.356 14.128], [49.693 15.381]]
+        # maxBounds: [[50.356 14.128], [49.693 15.381]]
 map
     ..setView [50.08, 14.4], 12
     ..addLayer L.tileLayer do
         "http://service.ihned.cz/tiles/urban/{z}/{x}/{y}.png"
         zIndex: 1 attribution: '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/" target = "_blank">CC BY-NC-SA 3.0 CZ</a> IHNED.cz, mapové data &copy; <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>, <a target="_blank" href="http://www.infoprovsechny.cz/request/aktuln_verze_gtfs#incoming-988">dopravní data</a> <a target="_blank" href="http://dpp.cz">DPP Praha</a>'
 (stationCoordinates) <~ $.getJSON './data/stationCoordinates.json'
+(stopDifferences) <~ $.getJSON './data/stopDifferences.json'
+# console.log Math.max ...stopDifferences # 4400
+# console.log Math.min ...stopDifferences # -8944
+
+color = d3.scale.linear!
+    .domain [-2000, 0,2000]
+    .range  ['#D7191C' '#FFFFBF' '#1A9641']
 window.stationCoordinates = stationCoordinates
+
+stationCoordinates.forEach (coord, id) ->
+    if coord
+        diff = stopDifferences[id]
+        # return if -50 < diff < 50
+        markerColor = color diff
+        new L.CircleMarker coord, fillColor: markerColor, color: \black, strokeOpacity: 0.5, strokeWidth: 0.5, fillOpacity: 0.9
+            ..addTo map
+
