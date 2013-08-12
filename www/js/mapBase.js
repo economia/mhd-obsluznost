1bc9d05d1a85279a007fa2af530abe50bdcf060c
Marcel Šulek <marcel@sulek.eu>
2013-08-12 14:59:43 +0200

    paralelizace nacitani JSONu

--- www/js/mapBase.js
+++ www/js/mapBase.js
@@ -1,5 +1,5 @@
 (function(){
-  var x$, this$ = this;
+  var x$, loadData, this$ = this;
   window.map = L.map('map', {
     fadeAnimation: false,
     minZoom: 6,
@@ -11,31 +11,34 @@
     zIndex: 1,
     attribution: '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/" target = "_blank">CC BY-NC-SA 3.0 CZ</a> IHNED.cz, mapové data &copy; <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>, <a target="_blank" href="http://www.infoprovsechny.cz/request/aktuln_verze_gtfs#incoming-988">dopravní data</a> <a target="_blank" href="http://dpp.cz">DPP Praha</a>'
   }));
-  $.getJSON('./data/stationCoordinates.json', function(stationCoordinates){
-    return $.getJSON('./data/stopDifferences.json', function(stopDifferences){
-      return $.getJSON('./data/stationNames.json', function(stationNames){
-        var color;
-        color = d3.scale.linear().domain([-2000, 0, 2000]).range(['#D7191C', '#FFFFBF', '#1A9641']);
-        window.stationCoordinates = stationCoordinates;
-        return stationCoordinates.forEach(function(coord, id){
-          var diff, markerColor, dir, icon, x$;
-          if (coord) {
-            diff = stopDifferences[id];
-            markerColor = color(diff);
-            dir = diff > 0 ? "více" : "méně";
-            icon = L.divIcon({
-              html: "<span style='background: " + markerColor + "' title='" + stationNames[id] + ": o " + Math.abs(diff) + " " + dir + " spojení'></span>",
-              iconSize: [15, 15],
-              className: "station-marker"
-            });
-            x$ = new L.marker(coord, {
-              icon: icon
-            });
-            x$.addTo(map);
-            return x$;
-          }
+  loadData = function(src, cb){
+    var this$ = this;
+    return $.getJSON(src, function(stationCoordinates){
+      return cb(null, stationCoordinates);
+    });
+  };
+  async.map(['./data/stationCoordinates.json', './data/stopDifferences.json', './data/stationNames.json'], loadData, function(err, arg$){
+    var stationCoordinates, stopDifferences, stationNames, color;
+    stationCoordinates = arg$[0], stopDifferences = arg$[1], stationNames = arg$[2];
+    color = d3.scale.linear().domain([-2000, 0, 2000]).range(['#D7191C', '#FFFFBF', '#1A9641']);
+    window.stationCoordinates = stationCoordinates;
+    return stationCoordinates.forEach(function(coord, id){
+      var diff, markerColor, dir, icon, x$;
+      if (coord) {
+        diff = stopDifferences[id];
+        markerColor = color(diff);
+        dir = diff > 0 ? "více" : "méně";
+        icon = L.divIcon({
+          html: "<span style='background: " + markerColor + "' title='" + stationNames[id] + ": o " + Math.abs(diff) + " " + dir + " spojení'></span>",
+          iconSize: [15, 15],
+          className: "station-marker"
         });
-      });
+        x$ = new L.marker(coord, {
+          icon: icon
+        });
+        x$.addTo(map);
+        return x$;
+      }
     });
   });
 }).call(this);
--- www/ls/mapBase.ls
+++ www/ls/mapBase.ls
@@ -9,9 +9,15 @@ map
     ..addLayer L.tileLayer do
         "http://service.ihned.cz/tiles/urban/{z}/{x}/{y}.png"
         zIndex: 1 attribution: '<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/" target = "_blank">CC BY-NC-SA 3.0 CZ</a> IHNED.cz, mapové data &copy; <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>, <a target="_blank" href="http://www.infoprovsechny.cz/request/aktuln_verze_gtfs#incoming-988">dopravní data</a> <a target="_blank" href="http://dpp.cz">DPP Praha</a>'
-(stationCoordinates) <~ $.getJSON './data/stationCoordinates.json'
-(stopDifferences) <~ $.getJSON './data/stopDifferences.json'
-(stationNames) <~ $.getJSON './data/stationNames.json'
+loadData = (src, cb) ->
+    (stationCoordinates) <~ $.getJSON src
+    cb null, stationCoordinates
+(err,[stationCoordinates, stopDifferences, stationNames]) <~ async.map do
+    *   './data/stationCoordinates.json'
+        './data/stopDifferences.json'
+        './data/stationNames.json'
+    *   loadData
+
 color = d3.scale.linear!
     .domain [-2000, 0,2000]
     .range  ['#D7191C' '#FFFFBF' '#1A9641']
